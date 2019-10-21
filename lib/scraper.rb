4f70590d7173fe68c94e350f684ffd13862c4c12
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-10-21 01:47:37 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,10 @@ class Scraper
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     binding.pry
+    doc.css(".post")
     doc.css(".post").firs.css("h2").text
+    doc.css(".post").first.css(".date").text
+    doc.css(".post").first.css("p").text
 
 
   def print_courses
